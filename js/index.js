8e9c9001da09b9a9117f960c27b372e5256cc749
udK01 <dominik.muranyi@gmail.com>
2024-01-20 19:06:59 +0000

    Arithmetics in JavaScript

--- js/index.js
+++ js/index.js
@@ -1,13 +1,54 @@
 // console.log("Hello World!");
-// console.log("I like kids.");
-// console.log("For legal reasons... that was a joke.");
 
 // window.alert("This is an alert!");
 // window.alert("Enable cookies?!");
 
+let x = 123;
+let price = 10.99;
+let animal = "monke";
+let online = true;
+
+console.log(`I ate ${x} 20 hamburgers`);
+console.log(`They cost ${price} each!`);
+console.log(`I am a ${animal}.`);
+console.log(`${animal} is online: ${online}`);
+
+console.log(typeof price);
+console.log(typeof animal);
+console.log(typeof online);
+
 document.getElementById("header1").textContent = "Peepee Content";
 document.getElementById("header1").style.color = "black";
 
-document.getElementById("paragraph1").textContent = "I love wiwis.";
+document.getElementById("paragraph1").textContent = "I love weewees.";
 document.getElementById("paragraph1").style.color = "black";
 
+//------------------------------------------------------------------
+
+let fullName = "Monke Myers";
+let age = 69;
+let isStudent = false;
+
+document.getElementById("p1").textContent = `They call me ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
+//------------------------------------------------------------------
+
+let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2; // Power of...
+// students = students % 2;
+
+// students += 2; Augmented versions...
+// ...
+
+console.log(students);
+
+//------------------------------------------------------------------
+
+// User Input
