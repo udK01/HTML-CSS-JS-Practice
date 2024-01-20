fccbcb7ba8d961680de2ee7d1e7bfb2430f8b264
udK01 <dominik.muranyi@gmail.com>
2024-01-20 18:46:05 +0000

    basic javascript

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// console.log("Hello World!");
+// console.log("I like kids.");
+// console.log("For legal reasons... that was a joke.");
+
+// window.alert("This is an alert!");
+// window.alert("Enable cookies?!");
+
+document.getElementById("header1").textContent = "Peepee Content";
+document.getElementById("header1").style.color = "black";
+
+document.getElementById("paragraph1").textContent = "I love wiwis.";
+document.getElementById("paragraph1").style.color = "black";
+
