124190787b3802645b80b5450bedd27db35bb4d9
udK01 <dominik.muranyi@gmail.com>
2024-01-20 19:24:17 +0000

    Type Conversion

--- js/index.js
+++ js/index.js
@@ -3,39 +3,39 @@
 // window.alert("This is an alert!");
 // window.alert("Enable cookies?!");
 
-let x = 123;
-let price = 10.99;
-let animal = "monke";
-let online = true;
+// let x = 123;
+// let price = 10.99;
+// let animal = "monke";
+// let online = true;
 
-console.log(`I ate ${x} 20 hamburgers`);
-console.log(`They cost ${price} each!`);
-console.log(`I am a ${animal}.`);
-console.log(`${animal} is online: ${online}`);
+// console.log(`I ate ${x} 20 hamburgers`);
+// console.log(`They cost ${price} each!`);
+// console.log(`I am a ${animal}.`);
+// console.log(`${animal} is online: ${online}`);
 
-console.log(typeof price);
-console.log(typeof animal);
-console.log(typeof online);
+// console.log(typeof price);
+// console.log(typeof animal);
+// console.log(typeof online);
 
-document.getElementById("header1").textContent = "Peepee Content";
-document.getElementById("header1").style.color = "black";
+// document.getElementById("header1").textContent = "Peepee Content";
+// document.getElementById("header1").style.color = "black";
 
-document.getElementById("paragraph1").textContent = "I love weewees.";
-document.getElementById("paragraph1").style.color = "black";
-
-//------------------------------------------------------------------
-
-let fullName = "Monke Myers";
-let age = 69;
-let isStudent = false;
-
-document.getElementById("p1").textContent = `They call me ${fullName}`;
-document.getElementById("p2").textContent = `You are ${age} years old`;
-document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+// document.getElementById("paragraph1").textContent = "I love weewees.";
+// document.getElementById("paragraph1").style.color = "black";
 
 //------------------------------------------------------------------
 
-let students = 30;
+// let fullName = "Monke Myers";
+// let age = 69;
+// let isStudent = false;
+
+// document.getElementById("p1").textContent = `They call me ${fullName}`;
+// document.getElementById("p2").textContent = `You are ${age} years old`;
+// document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
+//------------------------------------------------------------------
+
+// let students = 30;
 
 // students = students + 1;
 // students = students - 1;
@@ -47,19 +47,43 @@ let students = 30;
 // students += 2; Augmented versions...
 // ...
 
-console.log(students);
+// console.log(students);
 
 //------------------------------------------------------------------
 
 // User Input
 
-let username;
+// let username;
 
 // username = window.prompt("Enter Username");
 // console.log(`Whats up, ${username}?`);
 
-document.getElementById("submitBtn").onclick = function() {
-    username = document.getElementById("usernameTxt").value;
-    // console.log(`Welcome, ${username}`);
-    document.getElementById("myH1").textContent = `Hello ${username}!`
-}
+// document.getElementById("submitBtn").onclick = function() {
+//     username = document.getElementById("usernameTxt").value;
+//     // console.log(`Welcome, ${username}`);
+//     document.getElementById("myH1").textContent = `Hello ${username}!`
+// }
+
+//------------------------------------------------------------------
+
+// let age = window.prompt("How old are you?");
+// age = Number(age);
+// age+=1;
+
+// console.log(age, typeof age);
+
+// Type Conversion
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+//------------------------------------------------------------------
