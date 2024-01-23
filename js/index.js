aaaf843b5792a19a7deb9266322b4418ac0f269d
udK01 <dominik.muranyi@gmail.com>
2024-01-23 19:09:41 +0000

    Random Number Generator

--- js/index.js
+++ js/index.js
@@ -124,3 +124,33 @@ function changeValue(x) {
 document.getElementById("reset").onclick = function () {
   counter.textContent = "0";
 };
+
+//------------------------------------------------------------------
+
+// RANDOM NUMBER GENERATOR
+
+// const min = 50;
+// const max = 100;
+
+// let randomNum = Math.floor(Math.random() * (max - min)) + min;
+
+// console.log(randomNum);
+
+const myButton = document.getElementById("myButton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function () {
+  randomNum1 = Math.floor(Math.random() * (max - min) + min);
+  randomNum2 = Math.floor(Math.random() * (max - min) + min);
+  randomNum3 = Math.floor(Math.random() * (max - min) + min);
+  label1.textContent = randomNum1;
+  label2.textContent = randomNum2;
+  label3.textContent = randomNum3;
+};
