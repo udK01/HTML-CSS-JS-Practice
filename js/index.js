58f47324f050d2766bfdf624dee64ed3a6186e8c
udK01 <dominik.muranyi@gmail.com>
2024-01-24 20:49:26 +0000

    Strict Equality

--- js/index.js
+++ js/index.js
@@ -392,3 +392,42 @@ if (!isSunny) {
 } else {
   console.log(`Logical Operators: It is SUNNY!`);
 }
+
+//------------------------------------------------------------------
+
+// Strict Equality
+
+// = assign operator
+// == comparison operator (compare if vlaues are equal)
+// === strict equality operator (compare if values & datatype are equal)
+// != inequality operator
+// !== strict inequality operator
+
+if (PI == "3.14") {
+  console.log("Strict Equality: That is Pi.");
+} else {
+  console.log("Strict Equality: That is NOT Pi.");
+}
+
+if (PI === "3.14") {
+  // Number === String, hence false.
+  console.log("Strict Equality: That is Pi.");
+} else {
+  console.log("Strict Equality: That is NOT Pi.");
+}
+
+if (PI != "3.14") {
+  console.log("Strict Equality: That is NOT Pi.");
+} else {
+  console.log("Strict Equality: That is Pi.");
+}
+
+if (PI !== "3.14") {
+  console.log("Strict Equality: That is NOT Pi.");
+} else {
+  console.log("Strict Equality: That is Pi.");
+}
+
+//------------------------------------------------------------------
+
+// While Loops
