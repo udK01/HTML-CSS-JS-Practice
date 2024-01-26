2fc9cd777e9c14584cf847332f4ac02a09f0cbbd
udK01 <dominik.muranyi@gmail.com>
2024-01-26 22:23:05 +0000

    Rest Parameters In JavaScript

--- js/index.js
+++ js/index.js
@@ -689,3 +689,58 @@ console.log(`Spread Operator: Minimum Number: ${minimum}`);
 console.log(`Spread Operator: Spread Username: ${letters}`);
 console.log(`Spread Operator: New Fruits: ${newFruits}`);
 console.log(`Spread Operator: Foods: ${foods}`);
+
+//------------------------------------------------------------------
+
+// Rest Parameters
+
+// function openFridge(...foodsArr) {
+//   console.log(...foodsArr);
+//   // console.log(`Rest Parameters: ${foodsArr}`);
+// }
+
+// function getFood(...foodsArr1) {
+//   return foodsArr1;
+// }
+
+// const food1 = "pizza";
+// const food2 = "hamburger";
+// const food3 = "hotdog";
+// const food4 = "sushi";
+// const food5 = "ramen";
+
+// // openFridge(food1, food2, food3, food4, food5);
+
+// const foods1 = getFood(food1, food2, food3, food4, food5);
+
+// console.log(`Rest Parameters: ${foods1}`);
+
+// function sum(...numbers) {
+//   let result = 0;
+//   for (let n of numbers) {
+//     result += n;
+//   }
+//   return result;
+// }
+
+// function getAverage(...numbers) {
+//   let result = 0;
+//   for (let n of numbers) {
+//     result += n;
+//   }
+//   return result / numbers.length;
+// }
+
+// const total = sum(1, 2, 3, 4, 5);
+// console.log(`Rest Parameters: Your total is: £${total}`);
+
+// const average = getAverage(75, 100, 85, 90, 50);
+// console.log(`Rest Parameters: The class average was ${average}% on the exam.`);
+
+function combineStrings(...strings) {
+  return strings.join(" ");
+}
+
+const combinedName = combineStrings("Mr.", "Banana", "Circular", "III");
+
+console.log(`Rest Parameters: ${combinedName}`);
