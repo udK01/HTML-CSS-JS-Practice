0430d6e669f9ee14d9a832b39af1aa44d3eda484
udK01 <dominik.muranyi@gmail.com>
2024-01-26 20:09:40 +0000

    2D Arrays In JavaScript

--- js/index.js
+++ js/index.js
@@ -639,5 +639,36 @@ for (let i = fruits.length - 1; i >= 0; i--) {
 }
 
 for (let fruit of fruits) {
-  console.log(fruit);
+  console.log(`Arrays: ${fruit}`);
 }
+
+//------------------------------------------------------------------
+
+// 2D Arrays
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+matrix[0][0] = `X`;
+matrix[0][1] = `O`;
+matrix[0][2] = `X`;
+
+matrix[1][0] = `O`;
+matrix[1][1] = `X`;
+matrix[1][2] = `O`;
+
+matrix[2][0] = `X`;
+matrix[2][1] = `O`;
+matrix[2][2] = `X`;
+
+for (let row of matrix) {
+  const rowString = row.join(` `);
+  console.log(`2D Arrays: ${rowString}`);
+}
+
+//------------------------------------------------------------------
+
+// Spread Operator
