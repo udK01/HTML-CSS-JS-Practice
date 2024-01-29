1442508ec67ed7abc1011870dd1036c9ae85b668
udK01 <dominik.muranyi@gmail.com>
2024-01-29 12:57:30 +0000

    Filter Method - JavaScript

--- js/index.js
+++ js/index.js
@@ -992,3 +992,58 @@ console.log(`Map, Formatted Dates: ${formattedDates}`);
 //------------------------------------------------------------------
 
 // Filter
+
+// Example 1.
+
+// let fNumbers = [1, 2, 3, 4, 5, 6, 7];
+// let evenNumbers = fNumbers.filter(isEven);
+// let oddNumbers = fNumbers.filter(isOdd);
+
+// function isEven(element) {
+//   return element % 2 === 0;
+// }
+
+// function isOdd(element) {
+//   return element % 2 !== 0;
+// }
+
+// console.log(`Filter: ${oddNumbers}`);
+// console.log(`Filter: ${evenNumbers}`);
+
+// Example 2.
+
+// const ages = [16, 17, 18, 18, 19, 20, 60];
+// const adults = ages.filter(isAdult);
+// const children = ages.filter(isChild);
+
+// function isAdult(element) {
+//   return element >= 18;
+// }
+
+// function isChild(element) {
+//   return element < 18;
+// }
+
+// console.log(`Filter Children: ${children}`);
+// console.log(`Filter Adults: ${adults}`);
+
+// Example 3.
+
+const words = [`apple`, `orange`, `banana`, `kiwi`, `pomegranate`, `coconut`];
+const shortWords = words.filter(getShortWords);
+const longWords = words.filter(getLongWords);
+
+function getShortWords(element) {
+  return element.length <= 6;
+}
+
+function getLongWords(element) {
+  return element.length > 6;
+}
+
+console.log(`Filter: ${shortWords}`);
+console.log(`Filter: ${longWords}`);
+
+//------------------------------------------------------------------
+
+// Reduce
