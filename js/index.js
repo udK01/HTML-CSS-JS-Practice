d5301ffe04bbacb588d1e8f6e2d3301f92fd44a4
udK01 <dominik.muranyi@gmail.com>
2024-02-01 19:53:41 +0000

    Shuffle Js

--- js/index.js
+++ js/index.js
@@ -1856,4 +1856,16 @@ console.log(people);
 
 //------------------------------------------------------------------
 
-// Sorting
+// Shuffle - Fisher-Yates Algorithm
+
+const cards = [`A`, 2, 3, 4, 5, 6, 7, 8, 9, 10, `J`, `Q`, `K`];
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const random = Math.floor(Math.random() * (i + 1));
+    [array[i], array[random]] = [array[random], array[i]];
+  }
+}
+
+shuffle(cards);
+console.log(cards);
