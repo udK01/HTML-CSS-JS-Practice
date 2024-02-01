2b2222e8f561b02300ecaa6653c5246dca5818d7
udK01 <dominik.muranyi@gmail.com>
2024-02-01 18:18:43 +0000

    Getters & Setters

--- js/index.js
+++ js/index.js
@@ -1486,3 +1486,112 @@ hawk.fly();
 //------------------------------------------------------------------
 
 // Getters & Setters
+
+// Example 1
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  set width(newWidth) {
+    if (newWidth > 0) {
+      this._width = newWidth;
+    } else {
+      console.error(`Getters & Setters: Width must be a positive number!`);
+    }
+  }
+
+  set height(newHeight) {
+    if (newHeight > 0) {
+      this._height = newHeight;
+    } else {
+      console.error(`Getters & Setters: Height must be a positive number!`);
+    }
+  }
+
+  get width() {
+    return `Getters & Setters: ${this._width.toFixed(1)}cm`;
+  }
+
+  get height() {
+    return `Getters & Setters: ${this._height.toFixed(1)}cm`;
+  }
+
+  get area() {
+    return `Getters & Setters: ${(this._width * this._height).toFixed(1)}cm^2`;
+  }
+}
+
+const rectangle = new Rectangle(3, 4);
+
+// rectangle.width = -100000;
+// rectangle.height = `pizza`;
+
+console.log(`Getters & Setters: ${rectangle.width}`);
+console.log(`Getters & Setters: ${rectangle.height}`);
+console.log(`Getters & Setters: ${rectangle.area}`);
+
+// Example 2.
+
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  set firstName(newFirstName) {
+    if (typeof newFirstName === "string" && newFirstName.length > 0) {
+      this._firstName = newFirstName;
+    } else {
+      console.error(
+        `Getters & Setters: First Name Must Be A Non-Empty String!`
+      );
+    }
+  }
+
+  set lastName(newLastName) {
+    if (typeof newLastName === "string" && newLastName.length > 0) {
+      this._lastName = newLastName;
+    } else {
+      console.error(`Getters & Setters: Last Name Must Be A Non-Empty String!`);
+    }
+  }
+
+  set age(newAge) {
+    if (typeof newAge === "number" && newAge >= 0) {
+      this._age = newAge;
+    } else {
+      console.error(`Getters & Setters: Age Must Be A Non-Negative Number`);
+    }
+  }
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+
+  get fullName() {
+    return this._firstName + " " + this._lastName;
+  }
+
+  get age() {
+    return this._age;
+  }
+}
+
+const person = new Person(`Spongebob`, `Squarepants`, 30);
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.fullName);
+console.log(person.age);
+
+//------------------------------------------------------------------
+
+// Destructing
